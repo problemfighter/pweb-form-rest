a352f62750a48fd6b44624c6a879d2ae1afef60a
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2024-12-21 18:17:09 +0600

    Added New message

--- pweb_form_rest/common/pweb_com_fr_message.py
+++ pweb_form_rest/common/pweb_com_fr_message.py
@@ -2,3 +2,4 @@ class ComFRMessage:
     create_success = "Successfully created!"
     delete_success = "Successfully deleted!"
     update_success = "Successfully updated!"
+    data_validation_error = "Data Validation Error"
